c77a8ae00d3b4db883973a6c998e0108d327b885
forerojonathan <forerojonathan@gmail.com>
2020-10-20 19:48:41 -0500

    lesson-05: Includes, Exponent

---
+++ src/es7/lesson-05/index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * Includes
+ *
+ */
+
+const numbers = [1, 2, 3, 7, 8];
+console.log(numbers.includes(3));
+console.log(numbers.includes(3, 2));
+console.log(numbers.includes(3, 2));
+console.log(numbers.includes(30));
+
+/**
+ *
+ * Exponent
+ *
+ */
+
+const base = 4;
+const exponent = 3;
+const result = base ** exponent;
+
+console.log(result);
